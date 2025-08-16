84d91ccf25cc739b5babc4ab6eaca08199eaba55
sandeep23256 <sandeepmpct23@gmail.com>
2025-08-16 09:38:30 +0530

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,19 @@
-import { Toaster } from 'react-hot-toast';
-import './globals.css';
-import Providers from './providers';
-import LayoutWrapper from './user/Layout';
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
+import Providers from "./providers";
+import LayoutWrapper from "./user/Layout";
 
 export const metadata = {
-  title: 'PNINFOSYS',
-  description: 'Website by PNINFOSYS',
+  title: "PNINFOSYS",
+  description: "Website by PNINFOSYS",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body suppressHydrationWarning={true} className="flex flex-col min-h-screen">
+    <html lang="en" suppressHydrationWarning>
+      <body className="flex flex-col min-h-screen">
         <Providers>
-          <LayoutWrapper>
-            {children}
-          </LayoutWrapper>
+          <LayoutWrapper>{children}</LayoutWrapper>
         </Providers>
         <Toaster position="top-center" />
       </body>
